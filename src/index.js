e896a530f4f8be7f6fe21b11962e614a88320d49
Maria Saavedra <msaav3@gmail.com>
2017-11-13 12:13:43 -0600

    Added moment.js

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import './app/app.js';
 import swal from 'sweetalert2';
 import xml2js from 'xml2js';
 import lodash from 'lodash';
+import moment from 'moment';
 
 window.xml2js = xml2js;
 window.swal = swal;
+window.moment = moment;
 
 
 
